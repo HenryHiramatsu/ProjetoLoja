93842f79c76c927edfadaa784a8bb41a6a7068ad
Henry <hiramatsuhenry@gmail.com>
2025-09-10 08:04:46 -0300

    Adicionando as actions no CarrinhoController; Modificando o CarrinhoRepositorio; Adicionando os atributos da classe Produto

--- ProjetoLoja/Models/ItemCarrinho.cs
+++ ProjetoLoja/Models/ItemCarrinho.cs
@@ -1,6 +1,16 @@
-﻿namespace ProjetoLoja.Models
+﻿using LojaProdutosAsp.Models;
+
+namespace ProjetoLoja.Models
 {
     public class ItemCarrinho
     {
+        public int ProdutoId { get; set; }
+
+        public Produto Produto { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Preco { get; set; }
+
+
+        public decimal Total => Quantidade * Preco;
     }
 }
